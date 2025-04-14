f95ea8985c7e43bcd32940dc334bf40258c24b87
26nafis <spalanzaninafis@gmail.com>
2025-04-14 10:46:40 +0700

    mendeklarasikan array dengan ukuran dan diberi nilai

--- Array.cpp
+++ Array.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// mendeklarasikkan array tanpa ukuran, wajib diberi nilai awal
+int nilai []={1,2,3};
+
+//mendeklarasikan array dengan ukuran dan diberinilai awal
+string nama[5]={"ben" , "agus" , "fara" , "ama" , "akila"};
 
